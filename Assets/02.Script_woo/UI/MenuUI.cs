9923e8e6146efaaaed2feb471eae2a195fd94cd1
Wonhuijae <95402939+Wonhuijae@users.noreply.github.com>
2026-07-14 17:36:16 +0900

    콘텐츠 선택 UI 작업 중

--- Assets/02.Script_woo/UI/MenuUI.cs
+++ Assets/02.Script_woo/UI/MenuUI.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
     [SerializeField] CameraController controller;
     [SerializeField] EnvironmentManager KGS;
+    [SerializeField] Button Selected;
+    [SerializeField] Sprite SelectSprite;
+    [SerializeField] Sprite NormalSprite;
+
+    [SerializeField] List<MenuButton> SelectContentButtons = new();
+    [SerializeField] Dictionary<Button, MenuButton> Buttons = new();
+
+    private void Awake()
+    {
+        foreach (var button in SelectContentButtons)
+        {
+            Buttons[button.Button] = button;
+        }
+    }
 
     private void OnEnable()
     {
@@ -56,6 +75,27 @@ public class MenuUI : MonoBehaviour
     Application.Quit();
 #endif
     }
+
+    public void ClickButton(Button button)
+    {
+        if (Buttons.TryGetValue(button, out var sB))
+        {
+            if (Selected != null)
+            {
+                Buttons[Selected].Image.sprite = NormalSprite;
+                Buttons[Selected].Text.color = Color.black;
+            }
+
+            Selected = button;
+            sB.Image.sprite = SelectSprite;
+            sB.Text.color = Color.white;
+        }
+    }
+
+    public void OnSelect()
+    {
+        Buttons[Selected].OnExecute?.Invoke();
+    }
 }
 
 public static class SceneRequest
@@ -67,4 +107,13 @@ public class ContentRequest
 {
     public ESceneName LastScene;
     public int ContentID;
+}
+
+[System.Serializable]
+public class MenuButton
+{
+    public Button Button;
+    public Image Image;
+    public TMP_Text Text;
+    public UnityEvent OnExecute;
 }
