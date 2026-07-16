2a39b23f745ca42e4f1b9b071a8568c522d4bbf0
whj11200 <whj11200@naver.com>
2026-07-16 16:13:08 +0900

    시뮬레이션 관 입장 시 텍스트가 뜨도록 설정

--- Assets/02.Script_woo/TriggerZone/MiniatureSoundBox.cs
+++ Assets/02.Script_woo/TriggerZone/MiniatureSoundBox.cs
@@ -1,17 +1,52 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class MiniatureSoundBox : MonoBehaviour
 {
-    [SerializeField] AudioSource playersound;
-    [SerializeField] AudioClip sound;
-    
+    [SerializeField] GameObject KGS_Canvas;
+    [SerializeField] Image KGS_Img;
+    [SerializeField] TextMeshProUGUI KGS_Text;
+    [SerializeField] AudioSource KGS_Audio;
+    [SerializeField] AudioClip KGS_Clip;
+
+    bool isPlaying = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player")&& !isPlaying)
         {
-            
-            playersound.PlayOneShot(sound);
-            this.gameObject.SetActive(false);
+
+            StartStudyRoom();
+            isPlaying = true;
         }
+    }
+
+
+    public void StartStudyRoom()
+    {
+        StartCoroutine(StartUp());
+    }
+
+    private IEnumerator StartUp()
+    {
+        KGS_Canvas.SetActive(true);
+        KGS_Img.gameObject.SetActive(true);
+        KGS_Text.text = "이곳은 가상체험관입니다. \r\n벽면의 지도에서 지역을 선택하여 누출 및 화재 시뮬레이션을 시작할 수 있으며  \r\n뒤쪽의 문을 통해 관리소 및 통제소 작업자 학습관으로 이동할 수 있습니다";
+        
+        if (KGS_Audio != null && KGS_Clip != null)
+        {
+            KGS_Audio.PlayOneShot(KGS_Clip);
+
+            // 오디오 재생이 끝날 때까지 대기
+            yield return new WaitWhile(() => KGS_Audio.isPlaying);
+        }
+
+        KGS_Canvas.SetActive(false);
+        KGS_Img.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+
+        gameObject.SetActive(false);
     }
 }
