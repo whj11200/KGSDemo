856f2d6a9e1d2cc8424f253c59508b4786b27415
whj11200 <whj11200@naver.com>
2026-07-27 17:58:49 +0900

    미니어쳐 표 디자인수정 및 미니어쳐 대사 칸 조정

--- Assets/02.Script_woo/TriggerZone/MiniatureSoundBox.cs
+++ Assets/02.Script_woo/TriggerZone/MiniatureSoundBox.cs
@@ -33,7 +33,7 @@ public class MiniatureSoundBox : MonoBehaviour
     {
         KGS_Canvas.SetActive(true);
         KGS_Img.gameObject.SetActive(true);
-        KGS_Text.text = "이곳은 가상체험관입니다. \r\n벽면의 지도에서 지역을 선택하여 누출 및 화재 시뮬레이션을 시작할 수 있으며  \r\n뒤쪽의 문을 통해 관리소 및 통제소 작업자 학습관으로 이동할 수 있습니다";
+        KGS_Text.text = "이곳은 가상체험관입니다. \r\n벽면의 지도에서 지역을 선택하여 \r\n누출 및 화재 시뮬레이션을 시작할 수 있으며  \r\n뒤쪽의 문을 통해 \r\n관리소 및 통제소 작업자 학습관으로 \r\n이동할 수 있습니다";
         
         if (KGS_Audio != null && KGS_Clip != null)
         {
