a3451c567b9ec438a9e0fc73fb0d00d552b7c4b8
Wonhuijae <95402939+Wonhuijae@users.noreply.github.com>
2026-08-27 13:41:05 +0900

    IContentSimulation, SimulationBase 별도 파일로 분리

---
+++ Assets/02. Scripts/Abstract/SimulationBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SimulationBase : IContentSimulation
+{
+    protected ScenarioAsset Asset;
+    protected List<GameNode> GameNodes;
+    public ScenarioEventBus<ScenarioEventType> EventBus;
+
+    protected int CurrentNodeIndex = -1;
+    protected bool IsProcessBlocked = false;
+    protected bool IsRunning = false;
+
+    protected GameNode CurrentGameNode;
+
+    ScenarioEventBus<ScenarioEventType> IContentSimulation.EventBus => EventBus;
+
+    public int SimulNodeID { get => CurrentNodeIndex; }
+
+    public event Action OnSimulationCompleted;
+
+    public SimulationBase(ScenarioAsset asset)
+    {
+        Asset = asset;
+        GameNodes = new();
+        EventBus = new ScenarioEventBus<ScenarioEventType>();
+    }
+
+    public abstract void Initialize();
+    public virtual void StartSimulation()
+    {
+        ProcessSimulationStep();
+    }
+
+    public virtual void ProcessSimulationStep()
+    {
+        if (IsRunning) return;
+
+        IsRunning = true;
+
+        if (IsProcessBlocked)
+        {
+            IsRunning = false;
+            return;
+        }
+
+        if (CurrentNodeIndex >= 0)
+            GameNodes[CurrentNodeIndex].OnEnd?.Invoke();
+
+        CurrentNodeIndex++;
+
+        if (CurrentNodeIndex >= GameNodes.Count)
+        {
+            EndSimulation();
+            return;
+        }
+
+        GameNodes[CurrentNodeIndex]?.OnStart?.Invoke();
+        IsRunning = false;
+    }
+
+    public void CompleteStep()
+    {
+        if (!IsProcessBlocked)
+            return;
+
+        IsProcessBlocked = false;
+        ProcessSimulationStep();
+    }
+
+    protected void EndSimulation()
+    {
+        OnSimulationCompleted?.Invoke();
+    }
+}
---
+++ Assets/02. Scripts/IContentSimulation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public interface IContentSimulation
+{
+    public int SimulNodeID { get; }
+    ScenarioEventBus<ScenarioEventType> EventBus { get; }
+
+    public event Action OnSimulationCompleted;
+
+    public void Initialize();
+    public void StartSimulation();
+    public void CompleteStep();
+    public void ProcessSimulationStep();
+}
